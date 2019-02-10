c53569f3a33c6c4b26785067df3fea73fe86d2b9
pandeyashishkp <pandey.ashishkp@gmail.com>
2019-02-10 18:06:00 +0000

    added error checks in views.py for hpux

--- hpux/views.py
+++ hpux/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import HpuxForm
 from .models import Hpux
+import re
 
 
 def home(request):
@@ -21,7 +22,12 @@ def hpux_register_data(request):
     email = request.POST['email']
     date = request.POST['date']
     patch_bundle = request.POST['patch_bundle']
-    update = Hpux(servername=servername, change_number=change_number,
-                  email=email, date=date, patch_bundle=patch_bundle)
-    update.save()
-    return render(request, "hpux/register.html")
+    if not re.search(r'^CRQ[0-9]{10}$', change_number):
+        return render(request, "hpux/error.html", {'error': 'change_number is not in specified format- CRQ0000XXXXXX'})
+    elif not re.search(r'vodafone\.com$', email):
+        return render(request, "hpux/error.html", {'error': 'Please specify company email address'})
+    else:
+        update = Hpux(servername=servername, change_number=change_number,
+                      email=email, date=date, patch_bundle=patch_bundle)
+        update.save()
+        return render(request, "hpux/register.html")
